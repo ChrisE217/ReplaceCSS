3664a7839525ea4155e25c733be72cdf0e33aa18
Chris Ewart <chris.ewart1999@gmail.com>
2019-04-29 13:31:10 +0100

    codemirror config

--- js/replacecss.js
+++ js/replacecss.js
@@ -1,13 +1,20 @@
 
 var hmtlBox = CodeMirror(document.getElementById('html-input'), {
-    value: "<div class=\"btn btn-primary\">Button</div>",
-    mode: "text/html",
-    lineNumbers: true
+    value: "<div class=\"btn btn-primary\" style=\"color: blue;\">Button</div>",
+    mode: "htmlmixed",
+    lineNumbers: true,
+    theme: "dracula",
+    autofocus: true,
+    lineWrapping: true,
   });
 
   
 var cssBox = CodeMirror(document.getElementById('css-input'), {
     value: "body{\n\tbackground-color: blue;\n}",
-    mode: "text/html",
-    lineNumbers: true
+    mode: "css",
+    lineNumbers: true,
+    theme: "dracula",
+    readOnly: true,
+    lineWrapping: true,
   });
+
