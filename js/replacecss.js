cafd1920f0965b43a80533c7cc80858ad23c87cd
Chris Ewart <chris.ewart1999@gmail.com>
2019-05-04 17:16:03 +0100

    DOMparser

--- js/replacecss.js
+++ js/replacecss.js
@@ -1,33 +1,44 @@
 
 var inputHTMLBox = CodeMirror(document.getElementById('html-input'), {
-    value: "<div class=\"btn btn-primary\" style=\"color: blue;\">Button</div>",
-    mode: "htmlmixed",
-    lineNumbers: true,
-    theme: "dracula",
-    autofocus: true,
-    lineWrapping: true,
-  });
+  value: "<div class=\"btn btn-primary\" style=\"color: blue;\">Button</div>",
+  mode: "htmlmixed",
+  lineNumbers: true,
+  theme: "dracula",
+  autofocus: true,
+  lineWrapping: true,
+});
 
-  
 var outputCSSBox = CodeMirror(document.getElementById('home'), {
-    value: "body{\n\tbackground-color: blue;\n}",
-    mode: "css",
-    lineNumbers: true,
-    theme: "dracula",
-    readOnly: true,
-    lineWrapping: true,
-  });
+  value: "body{\n\tbackground-color: blue;\n}",
+  mode: "css",
+  lineNumbers: true,
+  theme: "dracula",
+  readOnly: true,
+  lineWrapping: true,
+});
 
-  var outputHTMLBox = CodeMirror(document.getElementById('profile'), {
-    value: "<div class=\"btn btn-primary\">Button</div>",
-    mode: "htmlmixed",
-    lineNumbers: true,
-    theme: "dracula",
-    readOnly: true,
-    lineWrapping: true,
-  });
+var outputHTMLBox = CodeMirror(document.getElementById('profile'), {
+  value: "<div class=\"btn btn-primary\">Button</div>",
+  mode: "htmlmixed",
+  lineNumbers: true,
+  theme: "dracula",
+  readOnly: true,
+  lineWrapping: true,
+});
 
 
-  $('#html-tab').click( () => {
-    $('#profile').attr('style', 'position: static;')
-  });
+$('#html-tab').click(() => {
+  $('#profile').attr('style', 'position: static;')
+});
+
+function startReplace() {
+  try {
+    //var parser = new DOMParser();
+    //var html = parser.parseFromString(inputHTMLBox.getValue(), 'text/html');
+    var htmlInput = inputHTMLBox.getValue();
+    console.log(htmlInput);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
