230a0ad5ff4d2b3f90cbc0561f4b63a59fd17749
Chris Ewart <chris.ewart1999@gmail.com>
2019-05-23 09:42:51 +0100

    use of id's and classes

--- js/replacecss.js
+++ js/replacecss.js
@@ -29,7 +29,7 @@ $("#html-tab").click(() => {
 });
 
 function getInput() {
-    
+
     try {
         let textInput = inputHTMLBox.getValue();
         if (textInput.length != 0) {
@@ -37,8 +37,8 @@ function getInput() {
             let htmlElements = parser.parseFromString(textInput, "text/html").body;
             const ids = document.getElementById("id-checkbox").checked;
             const classes = document.getElementById("class-checkbox").checked;
-            const parents = document.getElementById("parent-checkbox").checked;
-            extractStyles(ids, classes, parents, htmlElements);
+            // const parent = document.getElementById("parent-checkbox").checked;
+            extractStyles(ids, classes, parent, htmlElements);
         }
     } catch (e) {
         console.log(e);
@@ -49,7 +49,21 @@ function extractStyles(useID, useClass, useParent, input) {
     let cssOutput = [];
     input.querySelectorAll("body *").forEach((node) => {
         if (node.hasAttribute("style")) {
-            let style = (node.className + "\{\n\t" + node.getAttribute("style") + "\n\}")
+            let style = "";
+            if (useClass) {
+                style = ("." + node.className.split(" ").join(" .") + "\{\n\t" + node.getAttribute("style") + "\n\}");
+            }
+            if (useID && node.hasAttribute('id')) {
+                style = ("#" + node.id + "\{\n\t" + node.getAttribute("style") + "\n\}");
+            }
+            // if (useParent && node.parentNode.hasAttribute("style")) {
+            //     if (useClass) {
+            //         style = ("." + node.parentNode.className.split(" ").join(" .") + "\{\n\t" + node.parentNode.getAttribute("style") + "\n\}");
+            //     }
+            //     if (useID) {
+            //         style = ("#" + node.parentNode.id + "\{\n\t" + node.parentNode.getAttribute("style") + "\n\}");
+            //     }
+            // }
             node.removeAttribute("style");
             cssOutput.push(style);
         }
